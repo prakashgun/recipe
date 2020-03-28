3806e24eba87cb7df3bf4367368e3ca3c8e5e049
prakashgun <mail.prakash.india@gmail.com>
2020-03-28 12:33:15 +0530

    Some setting changes"

--- app/app/settings.py
+++ app/app/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '^(j&_xxk*57)$qf^!i3ad$z-u+x2o-avhw!e770l=u9n$_(zo7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 # Application definition
 
